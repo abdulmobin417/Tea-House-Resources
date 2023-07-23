ef26e91b4ec590a7bc3ba5c837ef2eacc663d0c7
AbdulMobin <abdulmobin417@gmail.com>
2023-07-24 00:04:24 +0600

    header section done

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      xs: "411px",
+      sm: "540px",
+      smd: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+      "3xl": "1920px",
+    },
+  },
+  plugins: [],
+};
+
+/*
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+*/
